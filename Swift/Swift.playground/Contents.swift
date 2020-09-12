59d3a658c71cfd3727501af65aabcfc1989c23ea
Rohan Aurora <rohanaurora@gmail.com>
2020-09-11 23:48:13 -0700

    Update revision doc.

--- Swift/Swift.playground/Contents.swift
+++ Swift/Swift.playground/Contents.swift
@@ -1,10 +1,5 @@
 import Foundation
 
-let rt = "oplg"
-var str = "Hello, playground \(rt)"
-
-let height: Int = 34
-
 let colors = Set(["red", "green", "blue"])
 var tup = (name: "Rohan", lname: "Aurora")
 
@@ -30,16 +25,6 @@ enum Result: String {
     }
 }
 
-let weather = "rain"
-
-switch weather {
-case "rain":
-    print("rain")
-case "sunny":
-    print("its sunny")
-default:
-    print("no weather")
-}
 
 //
 //outerloop: for i in 1...10 {
@@ -76,8 +61,161 @@ default:
 //}
 //
 //do {
-//    try makeSomeCall("Password")
+//    try makeSomeCall("password")
 //} catch {
 //    print("Check password!")
 //}
+
+//func simplyPrint(num: inout Int) {
+//    print(num)
+//}
+//
+//var z = 42
+//simplyPrint(num: &z)
+
+//
+//func changeNum(num: inout Int) {
+//    num = 2
+//    print(num)
+//}
+//
+//var variable:Int = 45
+//changeNum(num: &variable)
+
+
+//// Closures
+//let driving = { print("McLaren") }
+//driving()
+//
+//let driving2 = { (car:String) in
+//    print("I am driving \(car)")
+//}
+//driving2("McLaren!")
+//
+//let drivingWithReturn = { (car:String) -> String in
+//    return "I am driving \(car)"
+//}
+//print(drivingWithReturn("Tesla"))
+
+// Closure with params.
+//func travel(action:() -> Void) {
+//    print("Ready to roll!")
+//    action()
+//    print("Done")
+//}
+//
+//travel {
+//    print("Start McLaren")
+//}
+
+// Trailing closure syntax
+//travel() {
+//    print("Driving")
+//}
+
+//func raceReady(mode: (String) -> Void) {
+//    print("Ready")
+//    mode("start my car")
+//    print("Arrived")
+//}
+//
+//raceReady { (xyz: String) in
+//    print("I am going to \(xyz)")
+//}
+
+
+func groceryShopping(items: (String) -> [String]) {
+    print("Start shopping")
+    let i = items("Spinach")
+    for j in i {
+        print("Bought \(j)")
+    }
+    print("Finished")
+}
+
+let fruits = "Apples"
+
+groceryShopping { (veggies) -> [String] in
+    return [veggies, fruits]
+}
+
+groceryShopping {
+    [$0, fruits]
+}
+
+//func teaTasting(teaName: (String, String) -> String) {
+//    print("Go to the location")
+//    let tn = teaName("Mate", "South America")
+//    print(tn)
+//    print("Drink tea")
+//}
+//
+//teaTasting {  return "Open up \($0) from \($1)" }
+
+// Property Observers
+//struct Sport {
+//    var name: String
+//    var score: Int {
+//        didSet {
+//            print("Updated score - \(score)")
+//        }
+//    }
+//
+//    lazy var p = Player()
+//
+//    init(_ sName: String) {
+//        name = sName
+//        score = 0
+//    }
+//
+//    mutating func status() {
+//        print("The sport is \(name) and new score is \(score) by \(p.name)")
+//    }
+//
+//    mutating func scoreChange() {
+//        score = 40
+//    }
+//}
+//
+//var sp = Sport.init("Tennis")
+//sp.status()
+//sp.score = 20
+//sp.score = 0
+//sp.scoreChange()
+//sp.status()
+//
+//struct Player {
+//    var name: String
+//
+//    init() {
+//        name = "Djokovich"
+//    }
+//}
+
+// Making copies
+//struct Dog { // class point to same object in memory
+//    var name: String = "Bruno"
+//    var breed: String = "Lab"
+//}
+//
+//var d = Dog()
+//print(d.breed)
+//
+//var dCopy = d
+//dCopy.breed = "Doodle"
+//print(d.breed)
+
+// Protocol
+//protocol Bookable {
+//    var name: String { get set}
+//}
+//
+//struct User: Bookable {
+//    var name: String
+//}
+//
+//func bookData(data: Bookable) {
+//    print("The book name is \(data.name)")
+//}
+
 
