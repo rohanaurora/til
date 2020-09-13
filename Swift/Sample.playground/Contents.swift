a8a29e52251b5191a041cbbccb7e79bc5cc4d64c
Rohan Aurora <rohanaurora@gmail.com>
2020-09-12 23:43:01 -0700

    Add new examples.

---
+++ Swift/Sample.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+// Conditional Try
+//var content = ""
+//var url = URL(string: "http://www.yahoo.com")!
+//
+//content = (try? String(contentsOf: url)) ?? "Default"
+
+
+//func loadData() -> String {
+//    if let o = try? String(contentsOf: url) { return o }
+//    return "default"
+//}
+//
+//// Defer always called at the end of do block.
+//loadData: do {
+//    defer { print("Done")}
+//    for i in 1...10 {
+//        print(i)
+//    }
+//}
+
+//let array = Array(1...10)
+//let t = array.lazy.map({ $0 / 2 })
+//t.reduce(0, +)
+
