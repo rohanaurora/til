7c788e1dcb66e13f027be921ab3f2e533c440000
Rohan Aurora <rohanaurora@gmail.com>
2020-09-17 22:38:54 -0700

    Add more cases.

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -36,10 +36,40 @@ import Cocoa
 //g()
 //gCopy()
 
-let names = ["Michael Jackson", "Michael Collins", "Adele Adkins"]
-let o = names.filter { $0.hasPrefix("Michael")}
-print(o)
+//let names = ["Michael Jackson", "Michael Collins", "Adele Adkins"]
+//let o = names.filter { $0.hasPrefix("Michael")}
+//print(o)
+//
+//let words = ["is in", "Fearless"]
+//let input = "My album is in 1989"
+//let  t = words.contains(where: input.contains)
+//
+//let intA = [1,3,5,54]
+//intA.reduce(0) { (res, int1) in
+//    return res + int1
+//}
+//intA.reduce(0, +)
 
-let words = ["1989", "Fearless"]
-let input = "My album is in 1989"
-let  t = words.contains(where: <#T##(String) throws -> Bool#>)
+//var arrayClosures: [() -> Void] = []
+//
+//func process(_ input: @escaping() -> Void) {
+//    arrayClosures.append(input)
+//}
+//
+//process({print ("Hello1")})
+//process({print ("Hello2")})
+//
+//arrayClosures.forEach { (val) in
+//    val()
+//}
+
+//func executeCall(_ input:Int, completion: ([Int]?, Error?) -> Void) {
+//    print("You passed \(input)")
+//    let data = [2,3,40]
+//    completion(data, nil)
+//}
+//
+//executeCall(40) { (array, error) in
+//    print((array ?? []))
+//}
+
