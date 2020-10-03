46fef8b96820633b96d1332109494aab1fec5ceb
Rohan Aurora <rohanaurora@gmail.com>
2020-10-02 23:38:21 -0700

    Revise

---
+++ Swift/Tricks.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+var popo = 8_000_000
+print(popo)
+var str = "Hello, playground, \(popo)"
+print(str)
+
+let beatles:[String] = ["John", "Paul"]
+beatles[1]
+
+let colors = Set(["d", "red", "blue"])
+var tup = (32, "df")
+tup.0 = 43
+
+
+let dict:[String:Int] = ["Rohan":30, "Paul":38]
+print(dict)
